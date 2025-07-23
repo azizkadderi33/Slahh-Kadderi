bf74f9373a3ab648bc3ea670a972eccbdefab742
azizkadderi <kadderiaziz842@gmail.com>
2025-07-23 01:46:46 +0100

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
-  description: "Against changing Bot's nickname"
+  credits: "عبد العزيز",
+  description: "يمنع تغيير لقب البوت (نيرو)"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
@@ -12,9 +12,18 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     var { BOTNAME, ADMINBOT } = global.config;
     var { nickname } = await Threads.getData(threadID, botID);
     var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
+
+    if (
+      logMessageData.participant_id == botID &&
+      author != botID &&
+      !ADMINBOT.includes(author) &&
+      logMessageData.nickname != nickname
+    ) {
+        api.changeNickname(nickname, threadID, botID);
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+
+        return api.sendMessage({
+            body: `⛔ ${info.name}، متبدلش لقب البوت نيرو! راه غير الأدمين الرئيسي يقدر يبدلو.`
+        }, threadID);
     }  
-        }
+};
