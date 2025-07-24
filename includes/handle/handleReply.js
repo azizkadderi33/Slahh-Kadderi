7be7814311a384e009d8f3bd6788dc04a19cc0b8
azizkadderi <kadderiaziz842@gmail.com>
2025-07-24 03:38:16 +0100

    Update handleReply.js

--- includes/handle/handleReply.js
+++ includes/handle/handleReply.js
@@ -1,44 +1,69 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     return function ({ event }) {
         if (!event.messageReply) return;
-        const { handleReply, commands } = global.client
+
+        const { handleReply, commands } = global.client;
         const { messageID, threadID, messageReply } = event;
-        if (handleReply.length !== 0) {
-            const indexOfHandle = handleReply.findIndex(e => e.messageID == messageReply.messageID);
-            if (indexOfHandle < 0) return;
-            const indexOfMessage = handleReply[indexOfHandle];
-            const handleNeedExec = commands.get(indexOfMessage.name);
-            if (!handleNeedExec) return api.sendMessage(global.getText('handleReply', 'missingValue'), threadID, messageID);
-            try {
-                var getText2;
-                if (handleNeedExec.languages && typeof handleNeedExec.languages == 'object') 
-                	getText2 = (...value) => {
-                    const reply = handleNeedExec.languages || {};
-                    if (!reply.hasOwnProperty(global.config.language)) 
-                    	return api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', handleNeedExec.config.name), threadID, messengeID);
-                    var lang = handleNeedExec.languages[global.config.language][value[0]] || '';
-                    for (var i = value.length; i > -0x4 * 0x4db + 0x6d * 0x55 + -0x597 * 0x3; i--) {
+
+        if (handleReply.length === 0) return;
+
+        const indexOfHandle = handleReply.findIndex(e => e.messageID == messageReply.messageID);
+        if (indexOfHandle < 0) return;
+
+        const indexOfMessage = handleReply[indexOfHandle];
+        const handleNeedExec = commands.get(indexOfMessage.name);
+
+        if (!handleNeedExec) {
+            return api.sendMessage(global.getText('handleReply', 'missingValue'), threadID, messageID);
+        }
+
+        try {
+            let getText2;
+
+            if (handleNeedExec.languages && typeof handleNeedExec.languages === 'object') {
+                getText2 = (...value) => {
+                    const replyLang = handleNeedExec.languages || {};
+                    const currentLang = global.config.language;
+
+                    if (!replyLang.hasOwnProperty(currentLang)) {
+                        return api.sendMessage(
+                            global.getText('handleCommand', 'notFoundLanguage', handleNeedExec.config.name),
+                            threadID,
+                            messageID
+                        );
+                    }
+
+                    let lang = replyLang[currentLang][value[0]] || '';
+                    for (let i = 1; i < value.length; i++) {
                         const expReg = RegExp('%' + i, 'g');
                         lang = lang.replace(expReg, value[i]);
                     }
+
                     return lang;
                 };
-                else getText2 = () => {};
-                const Obj = {};
-                Obj.api = api
-                Obj.event = event 
-                Obj.models = models
-                Obj.Users = Users
-                Obj.Threads = Threads 
-                Obj.Currencies = Currencies
-                Obj.handleReply = indexOfMessage
-                Obj.models = models
-                Obj.getText = getText2
-                handleNeedExec.handleReply(Obj);
-                return;
-            } catch (error) {
-                return api.sendMessage(global.getText('handleReply', 'executeError', error), threadID, messageID);
+            } else {
+                getText2 = () => {};
             }
+
+            const Obj = {
+                api,
+                event,
+                models,
+                Users,
+                Threads,
+                Currencies,
+                handleReply: indexOfMessage,
+                getText: getText2
+            };
+
+            handleNeedExec.handleReply(Obj);
+            return;
+
+        } catch (error) {
+            return api.sendMessage(global.getText('handleReply', 'executeError', error), threadID, messageID);
         }
     };
-}
+};
+
+// 🛠️ بوت نيرو ☠️
+// المطور: عبد العزيز
